665e81b927d17fd1d94135afc7834acad3dbc16f
Kent Tokunaga <kenttokunaga@gmail.com>
2018-10-06 18:37:24 -0400

    Connect to MongoDB using mongoose

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-  PORT: process.env.PORT || 8080
+  PORT: process.env.PORT || 8080,
+  DATABASE_URL: process.env.DATABASE_URL
 };
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 /* eslint-disable no-console */
 const express = require('express');
+const mongoose = require('mongoose');
 const morgan = require('morgan');
 
-const { PORT } = require('./config');
+const { DATABASE_URL, PORT } = require('./config');
 
 const app = express();
 
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev'));
+
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true }, () => {
+  console.log('connected to mongodb');
+});
+console.log(DATABASE_URL);
 
 const server = app.listen(PORT, () => {
   console.info(`App listening on port ${server.address().port}`);
