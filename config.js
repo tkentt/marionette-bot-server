a6c38e9c36fc4f03f1e29c888fb4635a690c4a99
Kent Tokunaga <kenttokunaga@gmail.com>
2018-10-06 18:54:28 -0400

    Create user model

--- config.js
+++ config.js
@@ -2,5 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  DATABASE_URL: process.env.DATABASE_URL
+  DATABASE_URL: process.env.DATABASE_URL,
+  clientID: process.env.clientID,
+  clientSecret: process.env.clientSecret,
 };
---
+++ passport/discordStrategy.js
@@ -0,0 +1,11 @@
+// const passport = require('passport');
+// const DiscordStrategy = require('passport-discord').Strategy;
+// const { clientID, clientSecret } = require('./config');
+
+// passport.use(new DiscordStrategy({
+//   callbackURL: '/auth/discord/redirect',
+//   clientID,
+//   clientSecret
+// }, (accessToken, refreshToken, profile, done) => {
+
+// }));
