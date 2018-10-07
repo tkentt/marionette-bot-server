5e304e7896d33d1d1764f1182c027c468aead95c
Kent Tokunaga <kenttokunaga@gmail.com>
2018-10-07 01:38:48 -0400

    Implement user creation

--- passport/discordStrategy.js
+++ passport/discordStrategy.js
@@ -1,11 +1,25 @@
 const DiscordStrategy = require('passport-discord').Strategy;
 const { clientID, clientSecret } = require('../config');
+const User = require('../models/userModel');
 
 const discordStrategy = new DiscordStrategy({
   callbackURL: '/auth/discord/redirect',
   clientID,
   clientSecret
 }, (accessToken, refreshToken, profile, done) => {
+  User.findOne({ discordId: profile.id })
+    .then(user => {
+      if (!user) {
+        User.create({
+          username: profile.username,
+          discordId: profile.id
+        });
+      } else {
+        return user;
+      }
+    })
+    .then(user => console.log(user))
+    .catch(err => done(err));
   console.log(profile.id);
   done();
 });
