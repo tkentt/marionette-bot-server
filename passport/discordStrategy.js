73479d98b36b96fb58118f4dbb2655adf57e62c4
Kent Tokunaga <kenttokunaga@gmail.com>
2018-10-08 14:42:51 -0400

    serialize user

--- passport/discordStrategy.js
+++ passport/discordStrategy.js
@@ -1,6 +1,16 @@
+const passport = require('passport');
 const DiscordStrategy = require('passport-discord').Strategy;
 const { clientID, clientSecret } = require('../config');
 const User = require('../models/userModel');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findOne({ _id: id })
+    .then(user => done(null, user));
+});
 
 const discordStrategy = new DiscordStrategy({
   callbackURL: '/auth/discord/redirect',
@@ -9,16 +19,16 @@ const discordStrategy = new DiscordStrategy({
 }, (accessToken, refreshToken, profile, done) => {
   User.findOne({ discordId: profile.id })
     .then(user => {
-      if (!user) {
-        User.create({
+      if (user) {
+        return user;
+      } else {
+        return User.create({
           username: profile.username,
           discordId: profile.id
         });
-      } else {
-        return user;
       }
     })
-    .then(user => console.log(user))
+    .then(user => done(null, user))
     .catch(err => done(err));
   done();
 });
