983f45eba0ae7e2721067059915a419e12e66090
Kent Tokunaga <kenttokunaga@gmail.com>
2018-10-29 15:21:30 -0400

    Use graphql-yoga and reset index.js

--- src/index.js
+++ src/index.js
@@ -1,49 +1,52 @@
 /* eslint-disable no-console */
-import cors from 'cors';
-import express from 'express';
-import graphqlHTTP from 'express-graphql';
-import mongoose from 'mongoose';
-import morgan from 'morgan';
-import passport from 'passport';
+import { GraphQlServer } from 'graphql-yoga';
 
-const { DATABASE_URL, PORT } = require('../config');
-const authRouter = require('../router/auth-router');
-const discordStrategy = require('../passport/discord-strategy');
-const jwtStrategy = require('../passport/jwt-strategy');
-const schema = require('../schema/schema');
-const { initializeBot } = require('../bot');
 
-const app = express();
+// import cors from 'cors';
+// import express from 'express';
+// import graphqlHTTP from 'express-graphql';
+// import mongoose from 'mongoose';
+// import morgan from 'morgan';
+// import passport from 'passport';
 
-// Use cors
-app.use(cors());
+// const { DATABASE_URL, PORT } = require('../config');
+// const authRouter = require('../router/auth-router');
+// const discordStrategy = require('../passport/discord-strategy');
+// const jwtStrategy = require('../passport/jwt-strategy');
+// const schema = require('../schema/schema');
+// const { initializeBot } = require('../bot');
 
-// Passport stuff
-app.use(passport.initialize());
-passport.use(discordStrategy);
-passport.use(jwtStrategy);
+// const app = express();
 
-// Log all requests
-app.use(morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev'));
+// // Use cors
+// app.use(cors());
 
-// GraphQL endpoint
-app.use('/graphql', passport.authenticate('jwt', { session: false, failWithError: true }), graphqlHTTP((req) => ({
-  schema,
-  graphiql: true,
-  context: {
-    user: req.user,
-  }
-})));
+// // Passport stuff
+// app.use(passport.initialize());
+// passport.use(discordStrategy);
+// passport.use(jwtStrategy);
 
-// Routers
-app.use('/auth', authRouter);
+// // Log all requests
+// app.use(morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev'));
 
-if (require.main === module) {
-  mongoose.connect(DATABASE_URL, { useNewUrlParser:true })
-    .then(() => initializeBot())
-    .catch(err => console.error(err));
+// // GraphQL endpoint
+// app.use('/graphql', passport.authenticate('jwt', { session: false, failWithError: true }), graphqlHTTP((req) => ({
+//   schema,
+//   graphiql: true,
+//   context: {
+//     user: req.user,
+//   }
+// })));
 
-  app.listen(PORT, function() {
-    console.info(`Server listening on ${this.address().port}`);
-  }).on('error', err => console.error(err));
-}
+// // Routers
+// app.use('/auth', authRouter);
+
+// if (require.main === module) {
+//   mongoose.connect(DATABASE_URL, { useNewUrlParser:true })
+//     .then(() => initializeBot())
+//     .catch(err => console.error(err));
+
+//   app.listen(PORT, function() {
+//     console.info(`Server listening on ${this.address().port}`);
+//   }).on('error', err => console.error(err));
+// }
