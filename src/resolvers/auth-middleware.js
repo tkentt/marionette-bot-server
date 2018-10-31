4fd1fc63cb5b2fcaaf1fd63a6aff85165e6b02af
Kent Tokunaga <kenttokunaga@gmail.com>
2018-10-30 22:38:27 -0400

    Destructure user before passing to context

--- src/resolvers/auth-middleware.js
+++ src/resolvers/auth-middleware.js
@@ -2,7 +2,7 @@ import getUserId from '../utils/get-user-id';
 
 const authMiddleware = async(resolve, parent, args, context, info) => {
   const user = getUserId(context.request);
-  context.request.user = user;
+  context.request.user = { ...user };
   const result = await resolve(parent, args, context, info);
   return result;
 };
