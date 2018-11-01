3f57d72fa6411544d7f910d0f5e1517502faa893
Kent Tokunaga <kenttokunaga@gmail.com>
2018-11-01 00:27:18 -0400

    Use content as message

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -4,7 +4,7 @@ import sendMessage from '../bot/send-message';
 const Mutation = {
   createMessage(parent, args, { prisma, request }) {
     const data = { ...args.data, userId: request.user.discordId };
-    sendMessage(client, data.channelId, 'testing');
+    sendMessage(client, data.channelId, data.content);
     return prisma.mutation.createMessage({ data });
   },
 };
