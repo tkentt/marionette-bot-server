7557a2fb0a471bcc54fecf9c693dbc2e0d27e5d9
Kent Tokunaga <kenttokunaga@gmail.com>
2018-11-01 16:50:46 -0400

    Remove unused queries

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,27 +1,8 @@
 const Query = {
-  user(parent, args, { prisma }) {
+  user(parent, args, { prisma, request }, info) {
     return prisma.query.user({
-      where: { discordId: args.where.discordId }
-    });
-  },
-  users(parent, args, { prisma }, info) {
-    return prisma.query.users(null, info);
-  },
-  guild(parent, args, { prisma }) {
-    return prisma.query.guild({
-      where: { discordId: args.where.discordId }
-    });
-  },
-  guilds(parent, args, { prisma }, info) {
-    return prisma.query.guilds(null, info);
-  },
-  channel(parent, args, { prisma }) {
-    return prisma.query.channel({
-      where: { discordId: args.where.discordId }
-    });
-  },
-  channels(parent, args, { prisma }, info) {
-    return prisma.query.channels(null, info);
+      where: { discordId: request.user.discordId }
+    }, info);
   }
 };
 
