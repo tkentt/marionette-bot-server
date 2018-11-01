58cdbb69f712c0a9dc081538aa1126980f14166a
Kent Tokunaga <kenttokunaga@gmail.com>
2018-11-01 00:09:26 -0400

    Create createMessage mutation

--- src/bot/send-message.js
+++ src/bot/send-message.js
@@ -2,8 +2,9 @@ const sendMessage = async(client, channelId, message) => {
   const ready = client.readyAt;
   if (ready) {
     const channel = await client.channels.find('id', channelId);
-    return channel.send(message);
+    return channel ? channel.send(message) : null;
   }
+
   return null;
 };
 
--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,6 +1,12 @@
 import client from '../bot/client';
 import sendMessage from '../bot/send-message';
 
-const Mutation = {};
+const Mutation = {
+  createMessage(parent, args, { prisma, request }) {
+    const data = { ...args.data, userId: request.user.discordId };
+    sendMessage(client, data.channelId, 'testing');
+    return prisma.mutation.createMessage({ data });
+  },
+};
 
 export default Mutation;
