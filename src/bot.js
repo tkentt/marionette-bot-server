16bc013ebd12500924c917a8cd010c4cf010dd13
Kent Tokunaga <kenttokunaga@gmail.com>
2018-10-31 22:19:50 -0400

    Refactor seeding

--- src/bot.js
+++ src/bot.js
@@ -14,16 +14,15 @@ const startClient = () => {
   });
 };
 
-const seedData = async client => {
-  await upsertGuilds(client.guilds.array());
-};
-
 const upsertGuilds = async guilds => {
   for (let i=0; i<guilds.length; i++) {
     const guild = {
       discordId: guilds[i].id,
       name: guilds[i].name
     };
+    const channels = guilds[i].channels.array()
+      .filter(channel => channel.type === 'text');
+    const members = guilds[i].members.array();
 
     await prisma.mutation.upsertGuild({
       where: { discordId: guild.discordId },
@@ -31,45 +30,48 @@ const upsertGuilds = async guilds => {
       update: guild
     });
 
-    const channels = guilds[i].channels.array()
-      .filter(channel => channel.type === 'text');
-
-    for (let j=0; j<channels.length; j++) {
-      let channel = {
-        discordId: channels[j].id,
-        name: channels[j].name,
-        guild: {
-          connect: { discordId: guild.discordId }
-        }
-      };
-
-      await prisma.mutation.upsertChannel({
-        where: { discordId: channel.discordId },
-        create: channel,
-        update: channel
-      });
-    }
-
-    const members = guilds[i].members.array();
-
-    for (let k=0; k<members.length; k++) {
-      let member = {
-        discordId: members[k].user.id,
-        username: members[k].user.username,
-        guilds: {
-          connect: { discordId: guild.discordId }
-        }
-      };
-
-      await prisma.mutation.upsertUser({
-        where: { discordId: member.discordId },
-        create: member,
-        update: member
-      });
-    }
+    upsertGuildChannels(guild, channels);
+    upsertGuildMembers(guild, members);
   }
 
   return;
+};
+
+
+const upsertGuildChannels = async(guild, channels) => {
+  for (let j=0; j<channels.length; j++) {
+    let channel = {
+      discordId: channels[j].id,
+      name: channels[j].name,
+      guild: {
+        connect: { discordId: guild.discordId }
+      }
+    };
+
+    await prisma.mutation.upsertChannel({
+      where: { discordId: channel.discordId },
+      create: channel,
+      update: channel
+    });
+  }
+};
+
+const upsertGuildMembers = async(guild, members) => {
+  for (let k=0; k<members.length; k++) {
+    let member = {
+      discordId: members[k].user.id,
+      username: members[k].user.username,
+      guilds: {
+        connect: { discordId: guild.discordId }
+      }
+    };
+
+    await prisma.mutation.upsertUser({
+      where: { discordId: member.discordId },
+      create: member,
+      update: member
+    });
+  }
 };
 
 export default startClient;
